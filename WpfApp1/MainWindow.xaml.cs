28a36b4b0482cb9765d1dfcad07d90d0b011e43e
ScoRoss <rosslam5@live.com>
2024-01-18 10:05:33 +0000

    added second UI window to the program for later adding bindings

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
-﻿using Microsoft.Win32;  // Add this line
-
-using System.Text;
+﻿using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -35,5 +26,14 @@ namespace WpfApp1
                 MessageBox.Show($"Selected File: {selectedFilePath}");
             }
         }
+
+        private void BindButtonsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the BindButtonsWindow and show it
+            BindButtonsWindow bindButtonsWindow = new BindButtonsWindow();
+            bindButtonsWindow.Show();
+        }
+
+
     }
 }
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c992c72ebbf599db80a994bd0ef455e62ab5352f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ee5787cc9139e7a9af8480837342d2a5a12584")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
