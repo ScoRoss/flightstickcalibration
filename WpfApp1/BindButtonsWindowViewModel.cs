80535ed9b6a64f2af8bab01e8d8b13077320c641
ScoRoss <rosslam5@live.com>
2024-01-29 14:23:29 +0000

    ??

---
+++ WpfApp1/BindButtonsWindowViewModel.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1
+{
+    public class BindButtonsWindowViewModel : INotifyPropertyChanged
+    {
+        private string _selectedFilePath;
+
+        public string SelectedFilePath
+        {
+            get { return _selectedFilePath; }
+            set
+            {
+                _selectedFilePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BindButtonsWindowViewModel(string selectedFilePath)
+        {
+            SelectedFilePath = selectedFilePath;
+        }
+        
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
