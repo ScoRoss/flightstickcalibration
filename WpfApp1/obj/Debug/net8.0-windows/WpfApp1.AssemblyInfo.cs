c767a1eba4679f28142abd87ca50bd6925624616
ScoRoss <rosslam5@live.com>
2024-02-01 08:54:04 +0000

    added logo to repo , this is purley to make the logo look nice!

--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594de9db0922e1fac55f6d613099a1e0fb89dc0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8e0c96b50c98725b503dfa14aa8fcac581666a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
