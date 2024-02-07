936cd5b51e46106aa109e92f7708c28c30bad5d2
ScoRoss <rosslam5@live.com>
2024-02-07 16:59:02 +0000

    working ui that had full buttons

--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c767a1eba4679f28142abd87ca50bd6925624616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b1814f92bb377d371a35e64a0bead9c253036d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
